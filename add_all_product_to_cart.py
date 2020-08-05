3b4551c312715624cecf4f8a3b79c9a9e78192f9
Rohit Prasad <rohitprasad25061999@gmail.com>
2020-08-05 22:09:59 +0530

    Removed repeated code

--- add_all_product_to_cart.py
+++ add_all_product_to_cart.py
@@ -15,106 +15,80 @@ SCOPE:
 import time
 from selenium import webdriver
 
-
-def price_filteration(raw_price):
-    """
-        price_filteration is a function that takes all the price in the text format,
-        it will filter the actual price out by removing all irrelevant stuff & return it.
-    """
-
-    price = raw_price.split("Price:")[-1]
-    price = price.split("Rs.")[-1]
-    price = int(price)
-    return price
-
-
-def price_list_generator(product_list):
-    """
-        price_list_generator is a function that takes raw product price list,
-        manipulate all of them and return it.
-    """
-
-    price_list = []
-    for cost in product_list:
-        price = cost.text
-        filtered_price = price_filteration(price)
-        price_list.append(filtered_price)
-
-    return price_list
-
-
-def all_summer_product_price():
-    """
-        all_summer_product_price function will gonna collect all sunscreens product
-        price and return it.
-    """
-
-    product_list = driver.find_elements_by_xpath("//*[contains(text(),'SPF-50')\
-                                                 or contains(text(),'SPF-30')\
-                                                 or contains(text(),'SPF-40')\
-                                                 or contains(text(),'spf-30')\
-                                                 or contains(text(),'spf-40')\
-                                                 or contains(text(),'spf-50')]\
-                                                 /following-sibling::p")
-    return price_list_generator(product_list)
-
-
-def all_winter_product_price():
-    """
-        all_winter_product_price function will gonna collect all moisturizer product
-        price and return it.
-    """
-
-    product_list = driver.find_elements_by_xpath("//*[contains(text(),'Aloe')\
-                                                 or contains(text(),'Almond')\
-                                                 or contains(text(),'aloe')\
-                                                 or contains(text(),'almond')]\
-                                                 /following-sibling::p")
-    return price_list_generator(product_list)
-
-
-def adding_to_cart(season):
+def go_to_cart():
     """
         adding_to_cart function is gonna take season as the argument and based upon
         that it adds the product.
     """
-
-    if season == "sunscreens":
-        price_list = all_summer_product_price()
-    elif season == "moisturizers":
-        price_list = all_winter_product_price()
-
-    # Clicking the Add button for all product one by one
-    for price in price_list:
-        print("price to press:", price)
-        driver.find_element_by_xpath("//div[contains(@class,'col-4') and contains(.,'{}')]\
-                                     /descendant::button[text()='Add']".format(str(price))).click()
-        time.sleep(3)
-
-    print("Clicked the Add button of all products")
-
     go_to_cart_button = driver.find_element_by_xpath("//button[contains(text(),'Cart')]")
     go_to_cart_button.click()
 
     print("Clicked go to cart button successfully")
 
 
+def get_moisturizers_buttons():
+    """
+        get_moisturizers_buttons is a function that collects all the products and send all
+        to get clicked.
+    """
+
+    button_list = driver.find_elements_by_xpath("//*[contains(text(),'Aloe')\
+                                                 or contains(text(),'Almond')\
+                                                 or contains(text(),'aloe')\
+                                                 or contains(text(),'almond')]\
+                                                 /following-sibling::button")
+    click_add_product(button_list)
+
+
+def get_sunscreens_buttons():
+    """
+        get_sunscreens_buttons is a function that collects all the products and send all
+        to get clicked.
+    """
+
+    button_list = driver.find_elements_by_xpath("//*[contains(text(),'SPF-50')\
+                                                 or contains(text(),'SPF-30')\
+                                                 or contains(text(),'SPF-40')\
+                                                 or contains(text(),'spf-30')\
+                                                 or contains(text(),'spf-40')\
+                                                 or contains(text(),'spf-50')]\
+                                                 /following-sibling::button")
+    click_add_product(button_list)
+
+
+def click_add_product(button_list):
+    """
+        click_add_product is a function that takes button list as an input &
+        click out all.
+    """
+
+    for i in button_list:
+        i.click()
+        time.sleep(2)
+        print("successfully clicked!!!")
+
+
 def product_shopping(product_name):
     """
-        summer_product_shopping is a function that clicks out the buy sunscreens button &
-        adds all the respective product to the cart by calling funtion.
+        product_shopping is a function that's take product name as an input and clicks
+        the respective button and adds all the respective product to the cart by
+        calling funtion.
     """
 
     driver.find_element_by_xpath("//button[text()='Buy {}']".format(product_name)).click()
     print("Clicked Buy {} button successfully".format(product_name))
-    adding_to_cart(product_name)
+    if product_name == "sunscreens":
+        get_sunscreens_buttons()
+    else:
+        get_moisturizers_buttons()
+
     print("Yes!!! you have added all {} products to the cart:)".format(product_name))
 
 
 def shopping_decision(temperature):
     """
-        shopping_product is a function which will take current temperature
-        and based upon that, clicks the buy button.
+        shopping_decision is a function which will take current temperature
+        and will be taking decision what to buy.
     """
 
     if temperature < 19:
@@ -137,7 +111,6 @@ def finding_temperature():
     temperature = int(temperature[:-2])
     time.sleep(5)
     print("Current temperature is:",temperature)
-    print("lllllllllllllllll")
     return temperature
 
 
@@ -157,7 +130,8 @@ if __name__ == "__main__":
     current_temperature = finding_temperature()
     # Navigate to the respective website according to the current temperature
     shopping_decision(current_temperature)
-
+    #go to cart after adding all the products to the cart
+    go_to_cart()
     #Closing the page
     time.sleep(5)
     driver.close()
